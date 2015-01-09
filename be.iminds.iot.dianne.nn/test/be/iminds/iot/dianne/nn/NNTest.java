3d4a095e56d643e1c123255696d6b39f82798d28
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-01-09 22:29:45 +0100

    adapt NNTest to have actual tensor input/output

--- be.iminds.iot.dianne.nn/test/be/iminds/iot/dianne/nn/NNTest.java
+++ be.iminds.iot.dianne.nn/test/be/iminds/iot/dianne/nn/NNTest.java
@@ -1,7 +1,6 @@
 package be.iminds.iot.dianne.nn;
 
-import java.util.UUID;
-
+import org.junit.Before;
 import org.junit.Test;
 
 import be.iminds.iot.dianne.nn.module.activation.Tanh;
@@ -9,9 +8,19 @@ import be.iminds.iot.dianne.nn.module.container.Sequential;
 import be.iminds.iot.dianne.nn.module.io.Input;
 import be.iminds.iot.dianne.nn.module.io.Output;
 import be.iminds.iot.dianne.nn.module.layer.Linear;
+import be.iminds.iot.dianne.tensor.Tensor;
+import be.iminds.iot.dianne.tensor.TensorFactory;
 
 public class NNTest {
 
+	private TensorFactory factory;
+	
+	@Before
+	public void setUp(){
+		factory = TensorFactory.getFactory(TensorFactory.TensorType.JAVA);
+
+	}
+	
 	@Test
 	public void test() throws Exception {
 		Input in = new Input();
@@ -19,18 +28,22 @@ public class NNTest {
 		
 		Sequential nn = new Sequential();
 		nn.add(in);
-		nn.add(new Linear());
+		nn.add(new Linear(5,10));
 		nn.add(new Tanh());
-		nn.add(new Linear());
+		nn.add(new Linear(10,3));
 		nn.add(new Tanh());
 		nn.add(out);
 		
-		in.input(null);
+		Tensor input = factory.createTensor(5);
+		input.fill(1.0f);
+		in.input(input);
 		
 		Thread.sleep(1000);
 		System.out.println("============");
-		
-		out.expected(null);
+
+		Tensor expected = factory.createTensor(3);
+		expected.set(1.0f, 1);
+		out.expected(expected);
 		
 		Thread.sleep(1000);
 	}
