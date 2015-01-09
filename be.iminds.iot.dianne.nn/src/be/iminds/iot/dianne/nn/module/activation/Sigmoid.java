3497bd07b0d416c4c66be676f0533e54e97eeca9
Steven Bohez <steven.bohez@intec.ugent.be>
2015-01-09 22:15:32 +0100

    Fixed tensor allocation in activation package.

--- be.iminds.iot.dianne.nn/src/be/iminds/iot/dianne/nn/module/activation/Sigmoid.java
+++ be.iminds.iot.dianne.nn/src/be/iminds/iot/dianne/nn/module/activation/Sigmoid.java
@@ -4,25 +4,13 @@ import be.iminds.iot.dianne.nn.module.AbstractModule;
 
 public class Sigmoid extends AbstractModule {
 
-	public Sigmoid(){
-		// TODO should we allocate tensor here?
-		output = factory.createTensor(1);
-	}
-	
 	@Override
 	protected void forward() {
-		if(!input.sameDim(output)){
-			output = factory.createTensor(input.dims());
-		}
 		output = factory.getTensorMath().sigmoid(output, input);
 	}
 
 	@Override
 	protected void backward() {
-		// TODO check also if dim of output is same as gradoutput?
-		if(!gradInput.sameDim(gradOutput)){
-			gradInput = factory.createTensor(gradOutput.dims());
-		}
 		gradInput = factory.getTensorMath().cmul(gradInput, gradOutput, 
 				factory.getTensorMath().dsigmoid(gradInput, output));
 	}
--- be.iminds.iot.dianne.nn/src/be/iminds/iot/dianne/nn/module/activation/Tanh.java
+++ be.iminds.iot.dianne.nn/src/be/iminds/iot/dianne/nn/module/activation/Tanh.java
@@ -3,26 +3,14 @@ package be.iminds.iot.dianne.nn.module.activation;
 import be.iminds.iot.dianne.nn.module.AbstractModule;
 
 public class Tanh extends AbstractModule {
-
-	public Tanh(){
-		// TODO should we allocate tensor here?
-		output = factory.createTensor(1);
-	}
 	
 	@Override
 	protected void forward() {
-		if(!input.sameDim(output)){
-			output = factory.createTensor(input.dims());
-		}
 		output = factory.getTensorMath().tanh(output, input);
 	}
 
 	@Override
 	protected void backward() {
-		// TODO check also if dim of output is same as gradoutput?
-		if(!gradInput.sameDim(gradOutput)){
-			gradInput = factory.createTensor(gradOutput.dims());
-		}
 		// derivative of tanh:
 		// dtanh/dx = 1-tanh^2 
 		//
