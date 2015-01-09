965a20e12243f71e9c0f3dded22ae7ef13f0be89
Steven Bohez <steven.bohez@intec.ugent.be>
2015-01-09 22:15:49 +0100

    Implemented Linear module.

--- be.iminds.iot.dianne.nn/src/be/iminds/iot/dianne/nn/module/layer/Linear.java
+++ be.iminds.iot.dianne.nn/src/be/iminds/iot/dianne/nn/module/layer/Linear.java
@@ -1,17 +1,37 @@
 package be.iminds.iot.dianne.nn.module.layer;
 
 import be.iminds.iot.dianne.nn.module.AbstractModule;
+import be.iminds.iot.dianne.tensor.Tensor;
 
 public class Linear extends AbstractModule {
 
+	private Tensor weights;
+	private Tensor bias;
+	
+	private Tensor gradWeights;
+	private Tensor gradBias;
+	
+	public Linear(int inSize, int outSize){
+		super();
+		
+		weights = factory.createTensor(outSize, inSize);
+		bias = factory.createTensor(outSize);
+		
+		weights.rand();
+		bias.rand();
+	}
+	
 	@Override
 	protected void forward() {
-		System.out.println("FORWARD Linear "+id);
+		output = factory.getTensorMath().addmv(output, bias, weights, input);
 	}
 
 	@Override
 	protected void backward() {
-		System.out.println("BACKWARD Linear "+id);
+		gradInput = factory.getTensorMath().tmv(gradInput, weights, gradOutput);
+		
+		gradWeights = factory.getTensorMath().vv(gradWeights, gradOutput, input);
+		gradBias = gradOutput.clone(gradBias);
 	}
 
 }
