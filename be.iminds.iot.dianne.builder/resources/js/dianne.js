89b337e3cdbcfeed0182a96688df947b15152291
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-01-28 14:06:13 +0100

    basic NN module add and connect UI

--- be.iminds.iot.dianne.builder/resources/js/dianne.js
+++ be.iminds.iot.dianne.builder/resources/js/dianne.js
@@ -1,4 +1,43 @@
 
+var source = {
+	isSource:true,
+	anchor : "Right",	
+	paintStyle:{ 
+		strokeStyle:"#555", 
+		fillStyle:"#FFF", 
+		lineWidth:2 
+	},
+	hoverPaintStyle:{
+		lineWidth:3 
+	},			
+	connectorStyle:{
+		lineWidth:4,
+		strokeStyle:"#333",
+		joinstyle:"round",
+		outlineColor:"white",
+		outlineWidth:2
+	},
+	connectorHoverStyle:{
+		lineWidth:4,
+		strokeStyle:"#555",
+		outlineWidth:2,
+		outlineColor:"white"
+	},
+//		maxConnections:-1,
+}		
+// the definition of target endpoints (will appear when the user drags a connection) 
+var target = {
+	isTarget:true,
+	anchor: "Left",					
+	paintStyle:{ 
+		fillStyle:"#333"
+	},
+	hoverPaintStyle:{ 
+		fillStyle: "#555"
+	},
+//		maxConnections:-1,
+}
+
 jsPlumb.ready(function() {       
     // your jsPlumb related init code goes here
     console.log("init jsPlumb");
@@ -9,48 +48,8 @@ jsPlumb.ready(function() {
     	Connector : [ "Flowchart", { stub:[40, 60], gap:10, cornerRadius:5, alwaysRespectStubs:true } ],
     	DragOptions : { cursor: 'pointer', zIndex:2000 },
     });
-    
-	var source = {
-		isSource:true,
-		anchor : "Right",
-//		endpoint:"Rectangle",	
-		paintStyle:{ 
-			strokeStyle:"#555", 
-			fillStyle:"#FFF", 
-			lineWidth:2 
-		},
-		hoverPaintStyle:{
-			lineWidth:3 
-		},			
-		connectorStyle:{
-			lineWidth:4,
-			strokeStyle:"#333",
-			joinstyle:"round",
-			outlineColor:"white",
-			outlineWidth:2
-		},
-		connectorHoverStyle:{
-			lineWidth:4,
-			strokeStyle:"#555",
-			outlineWidth:2,
-			outlineColor:"white"
-		},
-//		maxConnections:-1,
-	},		
-	// the definition of target endpoints (will appear when the user drags a connection) 
-	target = {
-		isTarget:true,
-		anchor: "Left",
-//		endpoint:"Rectangle",					
-		paintStyle:{ 
-			fillStyle:"#333"
-		},
-		hoverPaintStyle:{ 
-			fillStyle: "#555"
-		},
-//		maxConnections:-1,
-	},			
-	init = function(connection) {			
+    		
+	var init = function(connection) {			
 		connection.getOverlay("label").setLabel(connection.sourceId.substring(15) + "-" + connection.targetId.substring(15));
 		connection.bind("editCompleted", function(o) {
 			if (typeof console != "undefined")
@@ -60,43 +59,63 @@ jsPlumb.ready(function() {
 
 	// suspend drawing and initialise.
 	jsPlumb.doWhileSuspended(function() {
-
-		jsPlumb.addEndpoint("Test", source);
-		jsPlumb.addEndpoint("Test2", target);
-		jsPlumb.addEndpoint("Test2", source);
 		
-					
 		// listen for new connections; initialise them the same way we initialise the connections at startup.
 		jsPlumb.bind("connection", function(connInfo, originalEvent) { 
 			init(connInfo.connection);
 		});			
 					
-		// make all the window divs draggable						
-		jsPlumb.draggable($(".module.draggable"), { grid: [20, 20] });		
+		// make all the window divs draggable (intially canvas will be empty so not needed...)						
+		// jsPlumb.draggable($(".module.draggable"), { grid: [20, 20] });		
 	
-		// connect a few up
-//		jsPlumb.connect({uuids:["Window2BottomCenter", "Window3TopCenter"], editable:true});
-        
 		//
-		// listen for clicks on connections, and offer to delete connections on click.
+		// listen for connection add/removes
 		//
-		jsPlumb.bind("click", function(conn, originalEvent) {
-			if (confirm("Delete connection from " + conn.sourceId + " to " + conn.targetId + "?"))
-				jsPlumb.detach(conn); 
-		});	
-		
-		jsPlumb.bind("connectionDrag", function(connection) {
-			console.log("connection " + connection.id + " is being dragged. suspendedElement is ", connection.suspendedElement, " of type ", connection.suspendedElementType);
-		});		
-		
-		jsPlumb.bind("connectionDragStop", function(connection) {
-			console.log("connection " + connection.id + " was dragged");
+		jsPlumb.bind("beforeDetach", function(connection) {
+			console.log("connection detach " + connection.sourceId + " -> " + connection.targetId);
+			// TODO check whether connection can be detached
+			return true;
 		});
-
-		jsPlumb.bind("connectionMoved", function(params) {
-			console.log("connection " + params.connection.id + " was moved");
+		
+		jsPlumb.bind("beforeDrop", function(connection) {
+			console.log("connection add " + connection.sourceId + " -> " + connection.targetId);
+			// TODO check whether connection is OK?
+			return true;
 		});
 	});
 
 });
 
+$( ".module.toolbox" ).click(function() {
+	var module = $( this ).clone().removeClass("toolbox").addClass("draggable").appendTo("#canvas");
+	// TODO configure module dialog?
+	var type = module.attr("id");
+	var id = guid();
+	module.attr("id",id);
+	if(type==="Input"){
+		jsPlumb.addEndpoint(module, source);
+	} else if(type==="Output"){
+		jsPlumb.addEndpoint(module, target);
+	} else {
+		jsPlumb.addEndpoint(module, source);
+		jsPlumb.addEndpoint(module, target);
+	}
+	
+	jsPlumb.draggable(module);
+});
+
+
+/**
+ * Generates a GUID string.
+ * @returns {String} The generated GUID.
+ * @example af8a8416-6e18-a307-bd9c-f2c947bbb3aa
+ * @author Slavik Meltser (slavik@meltser.info).
+ * @link http://slavik.meltser.info/?p=142
+ */
+function guid() {
+    function _p8(s) {
+        var p = (Math.random().toString(16)+"000000000").substr(2,8);
+        return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
+    }
+    return _p8() + _p8(true) + _p8(true) + _p8();
+}
