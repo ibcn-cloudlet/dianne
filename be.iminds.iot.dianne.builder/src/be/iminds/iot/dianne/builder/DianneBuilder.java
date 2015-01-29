33f9ab6df67865e607e787d89a55dfc72389047a
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-01-29 15:35:24 +0100

    cleanup builder servlet

--- be.iminds.iot.dianne.builder/src/be/iminds/iot/dianne/builder/DianneBuilder.java
+++ be.iminds.iot.dianne.builder/src/be/iminds/iot/dianne/builder/DianneBuilder.java
@@ -1,9 +1,9 @@
-package be.iminds.iot.diane.builder;
+package be.iminds.iot.dianne.builder;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -17,14 +17,13 @@ import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.http.HttpService;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
-
 import be.iminds.iot.dianne.nn.module.description.ModuleDescription;
 import be.iminds.iot.dianne.nn.module.description.ModuleProperty;
 import be.iminds.iot.dianne.nn.module.factory.ModuleFactory;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 @Component(service={javax.servlet.Servlet.class},
 	property={"alias:String=/dianne/builder"},
@@ -54,58 +53,59 @@ public class DianneBuilder extends HttpServlet {
 		}
 	}
 	
-	
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-	}
-	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String action = request.getParameter("action");
 
 		if(action.equals("available-modules")){
-			List<ModuleDescription> modules = new ArrayList<ModuleDescription>();
-			synchronized(factories){
-				for(ModuleFactory f : factories){
-					modules.addAll(f.getAvailableModules());
-				}
-			}
-			
-			JsonArray jsonModules = new JsonArray();
-			for(ModuleDescription module : modules){
-				jsonModules.add(new JsonPrimitive(module.getName()));
-			}
-			response.getWriter().write(jsonModules.toString());
-			response.getWriter().flush();
+			getAvailableModules(response.getWriter());
 		} else if(action.equals("module-properties")){
 			String type = request.getParameter("type");
-			
-			ModuleDescription module = null;
-			synchronized(factories){
-				for(ModuleFactory f : factories){
-					module = f.getModuleDescription(type);
-					if(module!=null)
-						break;
-				}
+			getModuleProperties(type, response.getWriter());
+		} 
+	}
+	
+	private void getAvailableModules(PrintWriter writer){
+		List<ModuleDescription> modules = new ArrayList<ModuleDescription>();
+		synchronized(factories){
+			for(ModuleFactory f : factories){
+				modules.addAll(f.getAvailableModules());
 			}
-			
-			if(module==null){
-				// return;
-			}
-			
-			JsonArray jsonProperties = new JsonArray();
-			for(ModuleProperty p : module.getProperties()){
-				JsonObject jsonProperty = new JsonObject();
-				jsonProperty.addProperty("id", p.getId());
-				jsonProperty.addProperty("name", p.getName());
-				
-				jsonProperties.add(jsonProperty);
-			}
-			
-			response.getWriter().write(jsonProperties.toString());
-			response.getWriter().flush();
 		}
+		
+		JsonArray jsonModules = new JsonArray();
+		for(ModuleDescription module : modules){
+			jsonModules.add(new JsonPrimitive(module.getName()));
+		}
+		writer.write(jsonModules.toString());
+		writer.flush();
+	}
+	
+	private void getModuleProperties(String type, PrintWriter writer){
+		ModuleDescription module = null;
+		synchronized(factories){
+			for(ModuleFactory f : factories){
+				module = f.getModuleDescription(type);
+				if(module!=null)
+					break;
+			}
+		}
+		
+		if(module==null){
+			// return;
+		}
+		
+		JsonArray jsonProperties = new JsonArray();
+		for(ModuleProperty p : module.getProperties()){
+			JsonObject jsonProperty = new JsonObject();
+			jsonProperty.addProperty("id", p.getId());
+			jsonProperty.addProperty("name", p.getName());
+			
+			jsonProperties.add(jsonProperty);
+		}
+		
+		writer.write(jsonProperties.toString());
+		writer.flush();
 	}
 }
