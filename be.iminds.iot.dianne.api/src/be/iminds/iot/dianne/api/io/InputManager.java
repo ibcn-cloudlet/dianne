ac84e09348e7bba18d1298839c71d023849e84f9
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-03-17 15:21:33 +0100

    add input/output manager apis

---
+++ be.iminds.iot.dianne.api/src/be/iminds/iot/dianne/api/io/InputManager.java
@@ -0,0 +1,15 @@
+package be.iminds.iot.dianne.api.io;
+
+import java.util.List;
+import java.util.UUID;
+
+// link Input modules to actual inputs
+public interface InputManager {
+
+	public List<String> getAvailableInputs();
+	
+	public void setInput(UUID inputId, String input);
+	
+	public void unsetInput(UUID inputId, String input);
+
+}
