10a97d081287053f7f64cd6628aa35812a7f3054
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-03-18 09:43:28 +0100

    add ForwardListener to input when attaching camera input

--- be.iminds.iot.dianne.builder/src/be/iminds/iot/dianne/builder/DianneInput.java
+++ be.iminds.iot.dianne.builder/src/be/iminds/iot/dianne/builder/DianneInput.java
@@ -1,24 +1,39 @@
 package be.iminds.iot.dianne.builder;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 
 import be.iminds.iot.dianne.api.io.InputManager;
+import be.iminds.iot.dianne.api.nn.module.ForwardListener;
+import be.iminds.iot.dianne.tensor.Tensor;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
 
 @Component(service = { javax.servlet.Servlet.class }, 
@@ -27,6 +42,18 @@ import com.google.gson.JsonPrimitive;
 public class DianneInput extends HttpServlet {
 	
 	private List<InputManager> inputManagers = Collections.synchronizedList(new ArrayList<InputManager>());
+
+	// Send event to UI when new input sample arrived
+	private AsyncContext sse = null;
+	private BundleContext context;
+	private Map<UUID, ServiceRegistration> inputListeners = Collections.synchronizedMap(new HashMap<UUID, ServiceRegistration>());
+	private JsonParser parser = new JsonParser();
+
+	
+	@Activate 
+	public void activate(BundleContext context){
+		this.context = context;
+	}
 	
 	@Reference(cardinality=ReferenceCardinality.MULTIPLE, 
 			policy=ReferencePolicy.DYNAMIC)
@@ -36,6 +63,19 @@ public class DianneInput extends HttpServlet {
 	
 	public void removeInputManager(InputManager mgr){
 		this.inputManagers.remove(mgr);
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// write text/eventstream response
+		response.setContentType("text/event-stream");
+		response.setHeader("Cache-Control", "no-cache");
+		response.setCharacterEncoding("UTF-8");
+		response.addHeader("Connection", "keep-alive");
+		
+		sse = request.startAsync();
+		sse.setTimeout(0); // no timeout => remove listener when error occurs.
 	}
 	
 	@Override
@@ -62,6 +102,40 @@ public class DianneInput extends HttpServlet {
 				for(InputManager m : inputManagers){
 					m.setInput(UUID.fromString(inputId), input);
 				}
+				
+				ForwardListener inputListener = new ForwardListener(){
+					@Override
+					public void onForward(Tensor output) {
+						if(sse!=null){
+							try {
+								JsonObject data = new JsonObject();
+
+								if(output.dims().length==3){
+									data.add("channels", new JsonPrimitive(output.dims()[0]));
+									data.add("height", new JsonPrimitive(output.dims()[1]));
+									data.add("width", new JsonPrimitive(output.dims()[2]));
+								} else {
+									data.add("channels", new JsonPrimitive(1));
+									data.add("height", new JsonPrimitive(output.dims()[0]));
+									data.add("width", new JsonPrimitive(output.dims()[1]));
+								}
+								data.add("data", parser.parse(Arrays.toString(output.get())));
+								
+								StringBuilder builder = new StringBuilder();
+								builder.append("data: ").append(data.toString()).append("\n\n");
+				
+								PrintWriter writer = sse.getResponse().getWriter();
+								writer.write(builder.toString());
+								writer.flush();
+							} catch(Exception e){}
+						}
+					}
+				};
+				Dictionary<String, Object> properties = new Hashtable<String, Object>();
+				properties.put("targets", new String[]{inputId});
+				properties.put("aiolos.unique", true);
+				ServiceRegistration r = context.registerService(ForwardListener.class.getName(), inputListener, properties);
+				inputListeners.put(UUID.fromString(inputId), r);
 			}
 		} else if("unsetinput".equals(action)){
 			String inputId = request.getParameter("inputId");
@@ -71,6 +145,11 @@ public class DianneInput extends HttpServlet {
 				for(InputManager m : inputManagers){
 					m.unsetInput(UUID.fromString(inputId), input);
 				}
+			}
+			
+			ServiceRegistration r = inputListeners.get(UUID.fromString(inputId));
+			if(r!=null){
+				r.unregister();
 			}
 		}
 	}
