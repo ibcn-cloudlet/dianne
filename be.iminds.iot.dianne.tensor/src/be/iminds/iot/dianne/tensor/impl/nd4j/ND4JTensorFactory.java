ed1d3d745bc9b47d5d83f0d924b8b0edf043ece3
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-04-23 14:07:06 +0200

    basic implementations for ND4J Tensor

--- be.iminds.iot.dianne.tensor/src/be/iminds/iot/dianne/tensor/impl/nd4j/ND4JTensorFactory.java
+++ be.iminds.iot.dianne.tensor/src/be/iminds/iot/dianne/tensor/impl/nd4j/ND4JTensorFactory.java
@@ -1,26 +1,32 @@
 package be.iminds.iot.dianne.tensor.impl.nd4j;
 
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
 import be.iminds.iot.dianne.tensor.TensorFactory;
 import be.iminds.iot.dianne.tensor.TensorMath;
 
 public class ND4JTensorFactory implements TensorFactory<ND4JTensor>{
 
+	public ND4JTensorFactory() {
+		Nd4j.factory().setOrder('c');
+	}
+	
 	@Override
 	public ND4JTensor createTensor(int... d) {
-		// TODO Auto-generated method stub
-		return null;
+		INDArray nd = Nd4j.create(d);
+		return new ND4JTensor(nd);
 	}
 
 	@Override
 	public ND4JTensor createTensor(float[] data, int... d) {
-		// TODO Auto-generated method stub
-		return null;
+		INDArray nd = Nd4j.create(data, d);
+		return new ND4JTensor(nd);
 	}
 
 	@Override
 	public TensorMath<ND4JTensor> getTensorMath() {
-		// TODO Auto-generated method stub
-		return null;
+		return new ND4JTensorMath();
 	}
 
 }
