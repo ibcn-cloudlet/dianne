213ca9cd7d72f1dab5f47d123a3ebda9fab30561
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-04-24 16:02:21 +0200

    build nd4j tensor lib as osgi bundle

--- be.iminds.iot.dianne.tensor/src/be/iminds/iot/dianne/tensor/impl/nd4j/ND4JTensorFactory.java
+++ be.iminds.iot.dianne.tensor/src/be/iminds/iot/dianne/tensor/impl/nd4j/ND4JTensorFactory.java
@@ -2,10 +2,12 @@ package be.iminds.iot.dianne.tensor.impl.nd4j;
 
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
+import org.osgi.service.component.annotations.Component;
 
 import be.iminds.iot.dianne.tensor.TensorFactory;
 import be.iminds.iot.dianne.tensor.TensorMath;
 
+@Component(property={"aiolos.export=false"})
 public class ND4JTensorFactory implements TensorFactory<ND4JTensor>{
 
 	public ND4JTensorFactory() {
