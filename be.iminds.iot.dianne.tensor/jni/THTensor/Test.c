2c630bf73f37d22bb5f93d0fff962ad2f97440a1
tverbele <tim.verbelen@intec.ugent.be>
2015-05-19 14:47:54 +0000

    some testing for getting cuda to work

--- be.iminds.iot.dianne.tensor/jni/THTensor/Test.c
+++ be.iminds.iot.dianne.tensor/jni/THTensor/Test.c
@@ -13,20 +13,27 @@
 int main(){
 	printf("Tensor: \n");
 
-	THTensor* tensor = THTensor_(newWithSize2d)(2, 2);
-	THTensor_(fill)(tensor, 1.0f);
-	real* data = THTensor_(data)(tensor);
+	THCState* state = (THCState*)malloc(sizeof(THCState));
+	THCudaInit(state);
 
-	printf(" %f %f \n %f %f \n", data[0], data[1], data[2], data[3]);
+	printf("Initialized CUDA %d %d \n", state->numDevices, state->numUserStreams);
 
-	THTensor_(add)(tensor, tensor, 1.0f);
+//	THTensor* tensor = THTensor_(newWithSize2d)(2, 2);
+//	THTensor_(fill)(tensor, 1.0f);
+//	real* data = THTensor_(data)(tensor);
+//
+//	printf(" %f %f \n %f %f \n", data[0], data[1], data[2], data[3]);
+//
+//	THTensor_(add)(tensor, tensor, 1.0f);
+//
+//	printf("Add 1: \n");
+//
+//	data = THTensor_(data)(tensor);
+//	printf(" %f %f \n %f %f \n", data[0], data[1], data[2], data[3]);
+//
+//	THTensor_(free)(tensor);
 
-	printf("Add 1: \n");
-
-	data = THTensor_(data)(tensor);
-	printf(" %f %f \n %f %f \n", data[0], data[1], data[2], data[3]);
-
-	THTensor_(free)(tensor);
-
+	THCudaBlas_shutdown(state);
+	free(state);
 	return 0;
 }
