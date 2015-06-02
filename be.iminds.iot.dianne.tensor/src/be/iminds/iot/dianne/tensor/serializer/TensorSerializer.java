0266ab60a55c4841a61ed59fd1c2bd5b076d87bc
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-06-02 13:04:52 +0200

    use new AIOLOS feature to allow serialization of one Tensor implementation and deserialize into another one with kryo

--- be.iminds.iot.dianne.tensor/src/be/iminds/iot/dianne/tensor/serializer/TensorSerializer.java
+++ be.iminds.iot.dianne.tensor/src/be/iminds/iot/dianne/tensor/serializer/TensorSerializer.java
@@ -11,7 +11,11 @@ import com.esotericsoftware.kryo.Serializer;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
 
-@Component(service=Serializer.class, property={"aiolos.export=false","kryo.serializer.class=be.iminds.iot.dianne.tensor.Tensor"})
+@Component(service=Serializer.class, property={"aiolos.export=false",
+	"kryo.serializer.class=be.iminds.iot.dianne.tensor.Tensor",
+	"kryo.deserializer.classes=be.iminds.iot.dianne.tensor.impl.java.JavaTensor,"
+	+ "be.iminds.iot.dianne.tensor.impl.nd4j.ND4JTensor,"
+	+ "be.iminds.iot.dianne.tensor.impl.th.THTensor"})
 public class TensorSerializer extends Serializer<Tensor> {
 
 	private TensorFactory factory;
