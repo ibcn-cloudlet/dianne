0b26d766527ab2cdfcd41fbeb89757526efd7f74
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-07-10 13:58:38 +0200

    add junit test for testing Frame module

---
+++ be.iminds.iot.dianne.nn/test/be/iminds/iot/dianne/nn/module/preprocessing/FrameTest.java
@@ -0,0 +1,77 @@
+package be.iminds.iot.dianne.nn.module.preprocessing;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.UUID;
+
+import javax.imageio.ImageIO;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import be.iminds.iot.dianne.api.nn.module.BackwardListener;
+import be.iminds.iot.dianne.api.nn.module.ForwardListener;
+import be.iminds.iot.dianne.tensor.Tensor;
+import be.iminds.iot.dianne.tensor.TensorFactory;
+import be.iminds.iot.dianne.tensor.impl.java.JavaTensorFactory;
+import be.iminds.iot.dianne.tensor.impl.th.THTensorFactory;
+import be.iminds.iot.dianne.tensor.util.ImageConverter;
+
+@RunWith(Parameterized.class)
+public class FrameTest {
+
+	private TensorFactory factory;
+	private ImageConverter converter;
+
+	public FrameTest(TensorFactory f, String name) {
+		this.factory = f;
+		this.converter = new ImageConverter(f);
+	}
+
+	@Parameters(name="{1}")
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] { 
+				{ new JavaTensorFactory(), "Java Tensor" },
+				{ new THTensorFactory(), "TH Tensor" }
+		});
+	}
+	
+	@Test
+	public void testFrame() throws Exception {
+	
+		Frame frame = new Frame(factory, 3, 231, 231);
+		
+		Tensor input = converter.readFromFile("test/snake.jpg");
+		converter.writeToFile("test/out.png", input);
+
+		Object lock = new Object();
+		frame.addForwardListener(new ForwardListener() {
+			
+			@Override
+			public void onForward(Tensor output) {
+				System.out.println(Arrays.toString(output.dims()));
+				try {
+					converter.writeToFile("test/framed-"+factory.getClass().getName()+".png", output);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				synchronized(lock){
+					lock.notifyAll();
+				}
+			}
+		});
+		long t1 = System.currentTimeMillis();
+		frame.forward(UUID.randomUUID(), input);
+		synchronized(lock){
+			lock.wait();
+		}
+		long t2 = System.currentTimeMillis();
+		System.out.println("Time "+(t2-t1)+" ms");
+	}
+}
