d694d05118b3d382bf82af1131196553a585eabd
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-08-19 11:56:43 +0200

    add RegularizationProcessor

---
+++ be.iminds.iot.dianne.nn.learn/src/be/iminds/iot/dianne/nn/learn/processors/RegularizationProcessor.java
@@ -0,0 +1,43 @@
+package be.iminds.iot.dianne.nn.learn.processors;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import be.iminds.iot.dianne.api.nn.learn.Processor;
+import be.iminds.iot.dianne.tensor.Tensor;
+
+/**
+ * Additional learning techniques like Momentum can be implemented as a Processor decorator
+ */
+public class RegularizationProcessor extends AbstractProcessor {
+
+	private final Processor decorated;
+	private final float regularization;
+	
+	private Map<UUID, Tensor> previousDelta = new HashMap<UUID, Tensor>();
+	
+	public RegularizationProcessor( AbstractProcessor p,
+			Map<String, String> config) {
+		super(p.factory, p.input, p.output, p.toTrain, p.dataset);
+		decorated = p;
+		
+		// TODO set regularization based on config
+		regularization = 0.001f;
+	}
+	
+	@Override
+	public float processNext() {
+		float error = decorated.processNext();
+		
+		// subtract previous parameters
+		toTrain.entrySet().stream().forEach(e -> {
+			Tensor params = e.getValue().getParameters();
+			Tensor deltaParams = e.getValue().getDeltaParameters();
+			factory.getTensorMath().sub(deltaParams, deltaParams, regularization, params);
+		});
+		
+		return error;
+	}
+
+}
