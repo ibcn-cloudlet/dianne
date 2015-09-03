98d990d494d82ec387b5b42f21d5e3e45f3efd50
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-09-03 18:49:59 +0200

    use DataLogger for printing in GreedyActionStrategy

--- be.iminds.iot.dianne.rl/src/be/iminds/iot/dianne/rl/agent/GreedyActionStrategy.java
+++ be.iminds.iot.dianne.rl/src/be/iminds/iot/dianne/rl/agent/GreedyActionStrategy.java
@@ -1,11 +1,12 @@
 package be.iminds.iot.dianne.rl.agent;
 
-import java.util.Arrays;
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
 
+import be.iminds.iot.dianne.api.log.DataLogger;
 import be.iminds.iot.dianne.tensor.Tensor;
 import be.iminds.iot.dianne.tensor.TensorFactory;
 
@@ -18,6 +19,9 @@ public class GreedyActionStrategy implements ActionStrategy {
 	private double epsilonMin = 0;
 	private double epsilonDecay = 1e-6;
 	
+	private DataLogger logger = null;
+	private String[] loglabels = new String[]{"Q0", "Q1", "Q2", "epsilon"};
+	
 	public Tensor selectActionFromOutput(Tensor output, long i) {
 		
 		Tensor action = factory.createTensor(output.size());
@@ -25,9 +29,10 @@ public class GreedyActionStrategy implements ActionStrategy {
 		
 		double epsilon = epsilonMin + (epsilonMax - epsilonMin) * Math.exp(-i * epsilonDecay);
 		
-		if(i % 1000 == 0)
-			System.out.println(i + "\tQ: " + Arrays.toString(output.get()) + "\te: " + epsilon);
-
+		if(logger!=null){
+			logger.log("AGENT", loglabels, output.get()[0],output.get()[1],output.get()[2], (float)epsilon);
+		}
+		
 		if (Math.random() < epsilon) {
 			action.set(1, (int) (Math.random() * action.size()));
 		} else {
@@ -59,4 +64,14 @@ public class GreedyActionStrategy implements ActionStrategy {
 	public void setTensorFactory(TensorFactory f){
 		this.factory =f;
 	}
+	
+	@Reference(cardinality = ReferenceCardinality.OPTIONAL)
+	public void setDataLogger(DataLogger l){
+		this.logger = l;
+		this.logger.setAlpha("epsilon", 1f);
+		this.logger.setAlpha("Q0", 1f);
+		this.logger.setAlpha("Q1", 1f);
+		this.logger.setAlpha("Q2", 1f);
+	}
+
 }
