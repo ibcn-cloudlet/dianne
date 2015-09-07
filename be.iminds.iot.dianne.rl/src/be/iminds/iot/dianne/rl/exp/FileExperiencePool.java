cbdec1454d0901f828b7d056bbab8d7a1dfd6b80
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-09-07 16:03:47 +0200

    fix FileExperiencePool (write to file in batch and use cycling arraylist)

--- be.iminds.iot.dianne.rl/src/be/iminds/iot/dianne/rl/exp/FileExperiencePool.java
+++ be.iminds.iot.dianne.rl/src/be/iminds/iot/dianne/rl/exp/FileExperiencePool.java
@@ -8,8 +8,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -20,7 +20,6 @@ import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Reference;
 
 import be.iminds.iot.dianne.api.dataset.Dataset;
-import be.iminds.iot.dianne.api.dataset.Sample;
 import be.iminds.iot.dianne.api.rl.ExperiencePool;
 import be.iminds.iot.dianne.api.rl.ExperiencePoolSample;
 import be.iminds.iot.dianne.tensor.Tensor;
@@ -43,7 +42,10 @@ public class FileExperiencePool implements ExperiencePool {
 	
 	private ReadWriteLock rwLock = new ReentrantReadWriteLock(true);
 	
-	private final LinkedList<ExperiencePoolSample> samples = new LinkedList<ExperiencePoolSample>();
+	// cycling arraylist of samples
+	private ArrayList<ExperiencePoolSample> samples;
+	private int start = 0; // current start index
+	private int size = 0;
 	
 	private File file;
 	private DataInputStream input;
@@ -60,6 +62,7 @@ public class FileExperiencePool implements ExperiencePool {
 		if(config.containsKey("maxSize"))
 			this.maxSize = (Integer) config.get("maxSize");
 
+		samples = new ArrayList<ExperiencePoolSample>(maxSize);
 		
 		// read from file 
 		try {
@@ -91,13 +94,18 @@ public class FileExperiencePool implements ExperiencePool {
 					
 					reward = input.readFloat();
 					
+					boolean terminate = false;
 					for(int i=0;i<stateSize;i++){
 						nextStateData[i] = input.readFloat();
+						if(nextStateData[i]==Float.MIN_VALUE){
+							terminate = true;
+							break;
+						}
 					}
 					
 					Tensor state = factory.createTensor(stateData, stateSize);
 					Tensor action = factory.createTensor(actionData, actionSize);
-					Tensor nextState = factory.createTensor(nextStateData, stateSize);
+					Tensor nextState = terminate ? null : factory.createTensor(nextStateData, stateSize);
 					
 					ExperiencePoolSample s = new ExperiencePoolSample(state, action, reward, nextState);
 					add(s);
@@ -115,27 +123,49 @@ public class FileExperiencePool implements ExperiencePool {
 	
 	@Override
 	public int size() {
-		return samples.size();
+		return size;
 	}
 	
 	@Override
 	public Tensor getInputSample(int index) {
-		return samples.get(index).state;
+		return samples.get(getIndex(index)).state;
 	}
 
 	@Override
 	public Tensor getState(int index) {
-		return samples.get(index).state;
+		return samples.get(getIndex(index)).state;
 	}
 
 	@Override
 	public Tensor getOutputSample(int index) {
-		return samples.get(index).action;
+		return samples.get(getIndex(index)).action;
 	}
 
 	@Override
 	public Tensor getAction(int index) {
-		return samples.get(index).action;
+		return samples.get(getIndex(index)).action;
+	}
+
+	@Override
+	public float getReward(int index) {
+		return samples.get(getIndex(index)).reward;
+	}
+
+	@Override
+	public Tensor getNextState(int index) {
+		return samples.get(getIndex(index)).nextState;
+	}
+	
+	public ExperiencePoolSample getSample(int index) {
+		return samples.get(getIndex(index));
+	}
+	
+	private int getIndex(int index){
+		int i = start + index;
+		if(i >= size){
+			i -= size;
+		}
+		return i;
 	}
 
 	@Override
@@ -149,68 +179,76 @@ public class FileExperiencePool implements ExperiencePool {
 	}
 
 	@Override
-	public float getReward(int index) {
-		return samples.get(index).reward;
-	}
-
-	@Override
-	public Tensor getNextState(int index) {
-		return samples.get(index).nextState;
-	}
-
-	@Override
 	public void addSample(Tensor state, Tensor action, float reward,
 			Tensor nextState) {
 		
 		ExperiencePoolSample s = new ExperiencePoolSample(state, action, reward, nextState);
 		add(s);
-
-		
-		// append to file
-		synchronized(file){
-			try {
-				float[] stateData = state.get();
-				for(int i=0;i<stateData.length;i++){
-					output.writeFloat(stateData[i]);
-				}
-				
-				float[] actionData = action.get();
-				for(int i=0;i<actionData.length;i++){
-					output.writeFloat(actionData[i]);
-				}
-				
-				output.writeFloat(reward);
-				
-				float[] nextStateData = nextState.get();
-				for(int i=0;i<nextStateData.length;i++){
-					output.writeFloat(nextStateData[i]);
-				}
-			
-			} catch(IOException e){
-				System.err.println("Failed to write sample to file for experience pool "+name);
-			}
-		}
+		write(s);
 	}
 	
 	@Override
 	public void addSamples(Collection<ExperiencePoolSample> ss) {
 		rwLock.writeLock().lock();
 		for(ExperiencePoolSample s : ss){
-			samples.add(s);
+			add(s);
 		}
-		while(samples.size() > maxSize){
-			samples.removeFirst();
+		rwLock.writeLock().unlock();
+		
+		synchronized(file){
+			for(ExperiencePoolSample s : ss){
+				write(s);
+			}
 		}
-		rwLock.writeLock().unlock();	
 	}
 	
 	private void add(ExperiencePoolSample s){
 		rwLock.writeLock().lock();
-		samples.add(s);
-		if(samples.size() > maxSize){
-			samples.removeFirst();
+		if(size==maxSize){
+			// cycling
+			int index = start++;
+			if(start==maxSize){
+				start = 0;
+			}
+			samples.set(index, s);
+		} else {
+			// just add to back
+			size++;
+			samples.add(s);
 		}
-		rwLock.writeLock().unlock();	
+		rwLock.writeLock().unlock();
+	}
+	
+	private void write(ExperiencePoolSample s){
+		// append to file
+		synchronized(file){
+			try {
+				float[] stateData = s.state.get();
+				for(int i=0;i<stateData.length;i++){
+					output.writeFloat(stateData[i]);
+				}
+				
+				float[] actionData = s.action.get();
+				for(int i=0;i<actionData.length;i++){
+					output.writeFloat(actionData[i]);
+				}
+				
+				output.writeFloat(s.reward);
+				
+				if(s.nextState!=null){
+					float[] nextStateData = s.nextState.get();
+					for(int i=0;i<nextStateData.length;i++){
+						output.writeFloat(nextStateData[i]);
+					}
+				} else {
+					output.writeFloat(Float.MIN_VALUE);
+				}
+				
+				output.flush();
+			} catch(IOException e){
+				System.err.println("Failed to write sample to file for experience pool "+name);
+			}
+		}
 	}
 	
 	@Reference
@@ -234,6 +272,8 @@ public class FileExperiencePool implements ExperiencePool {
 		
 		try {
 			samples.clear();
+			size = 0;
+			start = 0;
 
 			file.delete();
 			file = new File(dir+File.separator+"data.bin");
@@ -243,13 +283,26 @@ public class FileExperiencePool implements ExperiencePool {
 	}
 
 	@Override
-	public void setMaxSize(int size) {
+	public void setMaxSize(int max) {
 		rwLock.writeLock().lock();
-		// TODO if size gets bigger, load in samples from file?
-		maxSize = size;
-		while( samples.size() > maxSize){
-			samples.removeFirst();
+
+		// copy the samples
+		ArrayList<ExperiencePoolSample> newSamples = new ArrayList<ExperiencePoolSample>(maxSize);
+		
+		if(size>0){
+			int s = size - max;
+			s = s > 0 ? s : 0;
+			for(int i = s ; i< size; i++){
+				newSamples.add(getSample(i));
+			}
 		}
+		
+		start = 0;
+		size = size < max ? size : max;
+		samples = newSamples;
+		
+		maxSize = max;
+		
 		rwLock.writeLock().unlock();
 	}
 
