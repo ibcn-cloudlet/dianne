f93b2f8c7d8238baf412137185c717bd3e46d4a3
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-09-23 18:01:00 +0200

    mark train package as deprecated, should move to learn in next version

--- be.iminds.iot.dianne.api/src/be/iminds/iot/dianne/api/nn/train/package-info.java
+++ be.iminds.iot.dianne.api/src/be/iminds/iot/dianne/api/nn/train/package-info.java
@@ -1,4 +1,7 @@
 /**
  * This package contains all interfaces that are used for training a neural network.
+ * 
+ * This package is deprecated in favor of be.iminds.iot.dianne.api.learn
  */
+@java.lang.Deprecated
 package be.iminds.iot.dianne.api.nn.train;
