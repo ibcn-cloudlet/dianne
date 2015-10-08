98de5a430df258a53d64da8390ab96d0eef4f87a
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-10-08 16:09:24 +0200

    remove deprecated train API

--- be.iminds.iot.dianne.nn.train/src/be/iminds/iot/dianne/nn/train/eval/EvalProgressListener.java
+++
@@ -1,8 +0,0 @@
-package be.iminds.iot.dianne.nn.train.eval;
-
-import be.iminds.iot.dianne.tensor.Tensor;
-
-public interface EvalProgressListener {
-
-	public void onProgress(Tensor confusionMatrix);
-}
--- be.iminds.iot.dianne.nn.train/src/be/iminds/iot/dianne/nn/train/strategy/TrainProgressListener.java
+++
@@ -1,7 +0,0 @@
-package be.iminds.iot.dianne.nn.train.strategy;
-
-public interface TrainProgressListener {
-
-	public void onProgress(int epoch, int sample, float error);
-	
-}
