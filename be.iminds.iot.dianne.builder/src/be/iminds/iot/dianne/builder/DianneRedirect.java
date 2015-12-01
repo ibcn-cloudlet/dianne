50bf8377b0002c162cff41935d6b039b327562fe
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-12-01 16:57:06 +0100

    fix redirect in case of trailing slash

--- be.iminds.iot.dianne.builder/src/be/iminds/iot/dianne/builder/DianneRedirect.java
+++ be.iminds.iot.dianne.builder/src/be/iminds/iot/dianne/builder/DianneRedirect.java
@@ -18,7 +18,7 @@ public class DianneRedirect extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.sendRedirect("dianne/ui/dianne.html");
+		resp.sendRedirect("/dianne/ui/dianne.html");
 	}
 
 }
