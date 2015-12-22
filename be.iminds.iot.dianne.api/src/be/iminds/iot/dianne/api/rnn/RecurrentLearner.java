2cfd96d98733c7f2292f7da558867f1fe66f9648
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-12-22 10:56:14 +0100

    add rnn API for SequenceDataset and RecurrentLearner

---
+++ be.iminds.iot.dianne.api/src/be/iminds/iot/dianne/api/rnn/RecurrentLearner.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * DIANNE  - Framework for distributed artificial neural networks
+ * Copyright (C) 2015  iMinds - IBCN - UGent
+ *
+ * This file is part of DIANNE.
+ *
+ * DIANNE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *     Tim Verbelen, Steven Bohez
+ *******************************************************************************/
+package be.iminds.iot.dianne.api.rnn;
+
+import java.util.Map;
+import java.util.UUID;
+
+import be.iminds.iot.dianne.api.nn.learn.LearnProgress;
+import be.iminds.iot.dianne.api.nn.module.dto.NeuralNetworkInstanceDTO;
+
+/**
+ * The RecurrentLearner implements the flow of the learning process for training recurrent
+ * neural networks. The RecurrentLearner gets an instance of the recurrent neural network to 
+ * learn, instantiates the Processor to process items from the SequenceDataset, 
+ * and pushes weight updates to the DianneRepository.
+ * 
+ * @author tverbele
+ *
+ */
+public interface RecurrentLearner {
+
+	/**
+	 * @return uuid of this learner - same as the frameworkId this learner is deployed on
+	 */
+	UUID getLearnerId();
+	
+	/**
+	 * Start learning for a given neural network and dataset, using the given processor.
+	 * The learning will start in a background thread and this method immediately returns.
+	 * 
+	 * @param nni the neural network instance that should be updated
+	 * @param dataset the name of the dataset to process for learning
+	 * @param config the learner configuration to use
+	 */
+	void learn(NeuralNetworkInstanceDTO nni, String dataset, Map<String, String> config) throws Exception;
+	
+	/**
+	 * @return the current progress of the Learner
+	 */
+	LearnProgress getProgress();
+	
+	/**
+	 * @return whether or not this learner is busy
+	 */
+	boolean isBusy();
+	
+	/**
+	 * Stop the current learning session.
+	 */
+	void stop();
+}
---
+++ be.iminds.iot.dianne.api/src/be/iminds/iot/dianne/api/rnn/SequenceDataset.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * DIANNE  - Framework for distributed artificial neural networks
+ * Copyright (C) 2015  iMinds - IBCN - UGent
+ *
+ * This file is part of DIANNE.
+ *
+ * DIANNE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *     Tim Verbelen, Steven Bohez
+ *******************************************************************************/
+package be.iminds.iot.dianne.api.rnn;
+
+import be.iminds.iot.dianne.api.dataset.Dataset;
+import be.iminds.iot.dianne.tensor.Tensor;
+
+/**
+ * A SequenceDataset is a Dataset consisting out of a long sequence of entries.
+ * 
+ * This dataset is used to train recurrent neural networks, where the goal is to
+ * predict the next entry in the sequence from the history.
+ * 
+ * @author tverbele
+ *
+ */
+public interface SequenceDataset extends Dataset {
+	
+	/**
+	 * Returns the number of entries in the dataset
+	 * 
+	 * @return the number of entries in the dataset
+	 */
+	int size();
+
+	/**
+	 * Get a sample from the dataset
+	 * 
+	 * @param index the index to fetch, should be smaller than size()
+	 * @return the input sample at position index
+	 */
+	Tensor getInputSample(final int index);
+		
+	/**
+	 * Get the next sample from the dataset
+	 * 
+	 * @param index the index which next output is wanted, should be smaller than size()-1
+	 * @return the next sample, i.e. the sample at index+1
+	 */
+	Tensor getOutputSample(final int index);
+	
+	/**
+	 * Return a subset of the sequence, i.e. when training on sequences with limited length
+	 * @param index start index of the first input
+	 * @param length number of inputs requested
+	 * @return a sequence of length+1 items: length inputs + 1 corresponding output
+	 */
+	Tensor[] getSequence(final int index, final int length);
+	
+	/**
+	 * A human-readable name for this dataset
+	 * 
+	 * @return dataset name
+	 */
+	String getName();
+	
+	/**
+	 * Get human-readable names for the classes represented in an output vector
+	 * 
+	 * @return human-readable dataset labels
+	 */
+	String[] getLabels();
+}
