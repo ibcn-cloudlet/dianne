969eec7e029a6b0348d48bf33460209257676d49
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-12-22 11:35:09 +0100

    use BPTTLearner in RNN Commands

--- be.iminds.iot.dianne.rnn/src/be/iminds/iot/dianne/rnn/command/DianneRNNCommands.java
+++ be.iminds.iot.dianne.rnn/src/be/iminds/iot/dianne/rnn/command/DianneRNNCommands.java
@@ -22,23 +22,17 @@
  *******************************************************************************/
 package be.iminds.iot.dianne.rnn.command;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import be.iminds.iot.dianne.api.nn.Dianne;
 import be.iminds.iot.dianne.api.nn.NeuralNetwork;
-import be.iminds.iot.dianne.api.nn.learn.Criterion;
-import be.iminds.iot.dianne.api.nn.module.Trainable;
 import be.iminds.iot.dianne.api.nn.module.dto.NeuralNetworkInstanceDTO;
 import be.iminds.iot.dianne.api.nn.platform.DiannePlatform;
-import be.iminds.iot.dianne.nn.learn.criterion.MSECriterion;
+import be.iminds.iot.dianne.api.rnn.RecurrentLearner;
 import be.iminds.iot.dianne.tensor.Tensor;
 import be.iminds.iot.dianne.tensor.TensorFactory;
 
@@ -49,33 +43,32 @@ import be.iminds.iot.dianne.tensor.TensorFactory;
 		service=Object.class,
 		property={"osgi.command.scope=dianne",
 				  "osgi.command.function=generate",
-				  "osgi.command.function=bptt"},
+				  "osgi.command.function=bptt",
+				  "osgi.command.function=stopBptt"},
 		immediate=true)
 public class DianneRNNCommands {
 
 	private Dianne dianne;
 	private DiannePlatform platform;
+	private RecurrentLearner learner;
+
 	private TensorFactory factory;
 	
-	private final String chars = "abcde";
-	
-	public void generate(String nnName, char start, int n){
+	public void generate(String nnName, String start, int n){
 		// forward of a rnn
 		try {
 			NeuralNetworkInstanceDTO nni = platform.deployNeuralNetwork(nnName, "test rnn");
 			NeuralNetwork nn = dianne.getNeuralNetwork(nni).getValue();
-			try {
-				nn.loadParameters("test");
-			} catch(Exception e){
-				System.out.println("Failed loading parameters.");
-			}
 			
 			String result = ""+start;
-			char c = start;
+			
+			for(int i=0;i<start.length()-1;i++){
+				nextChar(nn, start.charAt(i));
+			}
+			
+			char c = start.charAt(start.length()-1);
 			for(int i=0;i<n;i++){
-				Tensor in = asTensor((char)c);
-				Tensor out = nn.forward(in);
-				c = asChar(out);
+				c = nextChar(nn, c);
 				result += c;
 			}
 			
@@ -87,127 +80,63 @@ public class DianneRNNCommands {
 		}
 	}
 	
-	public void bptt(String nnName){
-		bptt(nnName, 25, 1000);
-	}
-	
-	public void bptt(String nnName, int length, int iterations){
-		// training of a rnn by unfolding
+
+	public void bptt(String nnName, String dataset, String... properties){
 		try {
-			Criterion criterion = new MSECriterion(factory);
-			//Criterion criterion = new NLLCriterion(factory);
-			
-			Random rand = new Random();
-			String sequence = "aaabbbcccdddeeeaaabbbcccdddeeeaaabbbcccdddeeeaaabbbcccdddeeeaaabbbcccdddeeeaaabbbcccdddeeeaaabbbcccdddeee";
+			Map<String, String> config = createLearnerConfig(properties);
 			
 			NeuralNetworkInstanceDTO nni = platform.deployNeuralNetwork(nnName);
-			NeuralNetwork nn = dianne.getNeuralNetwork(nni).getValue();
-			
-			
-			// initialize parameters
-			nn.randomizeParameters();
-			nn.storeParameters("test");
-			
-			
-			float learningRate = 0.001f;
-			for(int l=0;l<iterations;l++){
-	
-				// load parameters
-				Map<UUID, Tensor> previousParameters = nn.loadParameters("test");
-				
-				// zero delta params
-				nn.getTrainables().values().stream().forEach(Trainable::zeroDeltaParameters);
-				
-				// keep all memories intermediate states
-				Map<UUID, Tensor[]> memories = new HashMap<UUID, Tensor[]>();
-				nn.getMemories().entrySet().forEach(e -> {
-					Tensor[] mems = new Tensor[length];
-					for(int i=0;i<length;i++){
-						mems[i] = e.getValue().getMemory().copyInto(null);
-					}
-					memories.put(e.getKey(), mems);
-				});
-				
-				// forward the sequence
-				int offset = rand.nextInt(20);
-				Tensor[] inputs = new Tensor[length];
-				Tensor[] outputs = new Tensor[length];
-				for(int i=0;i<length;i++){
-					// store input and memory
-					inputs[i] = asTensor(sequence.charAt(offset+i));
-					final int index = i;
-					nn.getMemories().entrySet().forEach(e ->{
-						e.getValue().getMemory().copyInto(memories.get(e.getKey())[index]);
-					});
-					
-					// forward
-					Tensor out = nn.forward(inputs[i]);
-					
-					// store output
-					outputs[i] = out;
-
-				}
-
-				
-				// backward
-				float error = 0;
-				for(int i=length-1;i>=0;i--){
-					Tensor target = asTensor(sequence.charAt(offset+i+1));
-					float err = criterion.error(outputs[i], target).get(0);
-					error+=err;
-					Tensor grad = criterion.grad(outputs[i], target);
-					
-					// first forward again with correct state and memories
-					final int index = i;
-					nn.getMemories().entrySet().forEach(e -> {
-						e.getValue().setMemory(memories.get(e.getKey())[index]);
-					});
-					nn.forward(inputs[i]);
-					
-					// TODO set grad to zero for all intermediates?
-					//if(i!=length-1){
-					//	grad.fill(0.0f);
-					//}
-					nn.backward(grad);
-					
-					// acc grad
-					nn.getTrainables().values().stream().forEach(m -> m.accGradParameters());
-					
-				}
-				System.out.println(error);
-
-				// apply learning rate
-				nn.getTrainables().values().stream().forEach(
-						m -> factory.getTensorMath().mul(m.getDeltaParameters(), m.getDeltaParameters(), -learningRate));
-				
-				// update parameters
-				nn.getTrainables().values().stream().forEach(Trainable::updateParameters);
-				
-				// store deltas
-				nn.storeDeltaParameters(previousParameters, "test");
-				
-			
-			}
-			
-			platform.undeployNeuralNetwork(nni);
-			
+			learner.learn(nni, dataset, config);
 		} catch(Exception e){
 			e.printStackTrace();
 		}
+	}
+
+	public void stopBptt(){
+		this.learner.stop();
+	}
+	
+	private Map<String, String> createLearnerConfig(String[] properties){
+		Map<String, String> config = new HashMap<String, String>();
+		// defaults
+		config.put("sequenceLength", "20");
+		config.put("criterion", "MSE");
+		config.put("learningRate", "0.01");
+		config.put("momentum", "0.9");
+
+		for(String property : properties){
+			String[] p = property.split("=");
+			if(p.length==2){
+				config.put(p[0].trim(), p[1].trim());
+			}
+		}
 		
+		return config;
 	}
 	
-	private Tensor asTensor(char c){
-		int index = chars.indexOf(c);
-		Tensor t = factory.createTensor(chars.length());
-		t.fill(0.0f);
-		t.set(1.0f, index);
-		return t;
-	}
 	
-	private char asChar(Tensor t){
-		int index = factory.getTensorMath().argmax(t);
-		return chars.charAt(index);
+	
+	private char nextChar(NeuralNetwork nn, char current){
+		// construct input tensor
+		String[] labels = nn.getOutputLabels();
+		Tensor in = factory.createTensor(labels.length);
+		in.fill(0.0f);
+		int index = 0;
+		for(int i=0;i<labels.length;i++){
+			if(labels[i].charAt(0)==current){
+				index = i;
+				break;
+			}
+		}
+		in.set(1.0f, index);
+		
+		// forward
+		Tensor out = nn.forward(in);
+		
+		// select next, for now arg max, better sample here?
+		int o = factory.getTensorMath().argmax(out);
+		
+		return labels[o].charAt(0);
 	}
 	
 	@Reference
@@ -223,5 +152,10 @@ public class DianneRNNCommands {
 	@Reference
 	void setDiannePlatform(DiannePlatform p){
 		this.platform = p;
+	}
+	
+	@Reference
+	void setRecurrentLearner(RecurrentLearner l){
+		this.learner = l;
 	}
 }
