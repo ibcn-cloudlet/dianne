c59d8c233cc1d27b9e5269355d9123be9c4252a4
Tim Verbelen <tim.verbelen@intec.ugent.be>
2016-02-08 16:11:26 +0100

    add dashboard mockup

--- be.iminds.iot.dianne.dashboard/resources/js/dianne-dashboard.js
+++ be.iminds.iot.dianne.dashboard/resources/js/dianne-dashboard.js
@@ -21,7 +21,140 @@
  *     Tim Verbelen, Steven Bohez
  *******************************************************************************/
 
-
 function submitJob(){
 	window.alert("Submit a job!");
 }
+
+
+// placeholder charts
+$(function () {
+
+    $(document).ready(function () {
+    	
+    	
+    	
+    	// gradient fill for pie chart
+    	 Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function (color) {
+    	        return {
+    	            radialGradient: {
+    	                cx: 0.5,
+    	                cy: 0.3,
+    	                r: 0.7
+    	            },
+    	            stops: [
+    	                [0, color],
+    	                [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+    	            ]
+    	        };
+    	    });
+    	
+        // status chart
+        $('#status-chart').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                height: 220,
+                type: 'pie'
+            },
+            legend: {
+                layout: 'horizontal',
+                floating: true,
+                x: -10,
+                y: 10
+            },
+            title:{
+                text:''
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.y}</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                name: 'Utilization',
+                colorByPoint: true,
+                data: [{
+                    name: 'Learning',
+                    y: 10
+                }, {
+                    name: 'Idle',
+                    y: 5
+                }, {
+                    name: 'Evaluating',
+                    y: 2
+                }]
+            }]
+        });
+        
+        
+        
+        $('#chart1').highcharts({
+            chart: {
+                height: 250,
+                type: 'line'
+            },
+            title: {
+                text: ''
+            },
+            xAxis: {
+                categories: ['100k', '200k', '300k', '400k', '500k', '600k', '700k', '800k', '900k', '1M']
+            },
+            yAxis: {
+            },
+            legend: {
+            	enabled: false
+            },
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.series.name + '</b><br/>' +
+                        this.x + ': ' + this.y;
+                }
+            },
+            plotOptions: {
+            },
+            series: [{
+            	name: 'Error',
+                data: [2.1, 1.2, 0.8, 0.65, 0.51, 0.43, 0.31, 0.28, 0.24, 0.22]
+            }]
+        });
+        
+        $('#chart2').highcharts({
+            chart: {
+                height: 250,
+                type: 'line'
+            },
+            title: {
+                text: ''
+            },
+            xAxis: {
+                categories: ['100k', '200k', '300k', '400k', '500k', '600k', '700k', '800k', '900k', '1M']
+            },
+            yAxis: {
+            },
+            legend: {
+            	enabled: false
+            },
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.series.name + '</b><br/>' +
+                        this.x + ': ' + this.y;
+                }
+            },
+            plotOptions: {
+            },
+            series: [{
+            	name: 'Target Q',
+                data: [-0.1, -0.5, -0.8, -0.7, -0.4, -0.2, 0.4]
+            }]
+        });
+    });
+});
