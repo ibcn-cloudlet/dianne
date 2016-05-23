11e9e4c4749ca46bd5d33b8c78b83bb44bcb9b95
Tim Verbelen <tim.verbelen@intec.ugent.be>
2016-05-23 08:59:04 +0200

    generate random color in UI for > 4 nodes

--- be.iminds.iot.dianne.builder/resources/js/dianne.js
+++ be.iminds.iot.dianne.builder/resources/js/dianne.js
@@ -298,9 +298,30 @@ var colors = ['#FF6CDA','#81F781','#AC58FA','#FA5858'];
 var colorIndex = 0;
 
 function nextColor(){
+	if(colorIndex >= colors.length){
+		// generate random color
+		return randomColor();
+	}
 	return colors[colorIndex++];
 }
 
+function randomColor() {
+    var r, g, b;
+    var h = Math.random();
+    var i = ~~(h * 6);
+    var f = h * 6 - i;
+    var q = 1 - f;
+    switch(i % 6){
+        case 0: r = 1; g = f; b = 0; break;
+        case 1: r = q; g = 1; b = 0; break;
+        case 2: r = 0; g = 1; b = f; break;
+        case 3: r = 0; g = q; b = 1; break;
+        case 4: r = f; g = 0; b = 1; break;
+        case 5: r = 1; g = 0; b = q; break;
+    }
+    var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
+    return (c);
+}
 
 
 /*
