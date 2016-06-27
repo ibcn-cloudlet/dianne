adc23e2163be6322887fb0e854f81c21fce5ca28
ejodconi <elias.deconinck@intec.ugent.be>
2016-06-27 05:43:37 -0600

    support older cmake versions upto 2.8.12

--- be.iminds.iot.dianne.tensor.native/jni/cmake/FindTorch.cmake
+++ be.iminds.iot.dianne.tensor.native/jni/cmake/FindTorch.cmake
@@ -2,14 +2,14 @@
 #
 # Torch_FOUND -- set to 1 if found
 SET(Torch_FOUND 1)
-get_filename_component(TH_DIRECTORY "torch/torch7/lib/TH" REALPATH BASE_DIR ${CMAKE_CURRENT_LIST_DIR}/../)
+get_filename_component(TH_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/../torch/torch7/lib/TH" REALPATH)
 MESSAGE(STATUS "TH directory: ${TH_DIRECTORY}")
 set(THNN_INSTALL_LIB_SUBDIR ${CMAKE_CURRENT_SOURCE_DIR}/install)
 set(Torch_INSTALL_LIB ${TH_DIRECTORY}/build)
 INCLUDE_DIRECTORIES(${TH_DIRECTORY} ${TH_DIRECTORY}/build)
 
 IF(CUDA)
-    get_filename_component(THC_DIRECTORY "cutorch/cutorch/lib/THC" REALPATH BASE_DIR ${CMAKE_CURRENT_LIST_DIR}/../)
+    get_filename_component(THC_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/../cutorch/cutorch/lib/THC" REALPATH)
     MESSAGE(STATUS "THC directory: ${THC_DIRECTORY}")
     INCLUDE_DIRECTORIES(${THC_DIRECTORY}/.. ${THC_DIRECTORY} ${THC_DIRECTORY}/build)
 ENDIF(CUDA)
