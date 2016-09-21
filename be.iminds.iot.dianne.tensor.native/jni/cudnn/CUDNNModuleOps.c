a7dff5f85b19b787e78764a58c306210ebd3240d
Tim Verbelen <tim.verbelen@intec.ugent.be>
2016-09-21 11:26:52 +0200

    initiate cudnn dir and Makefile to replace selected ModuleOps symbols

---
+++ be.iminds.iot.dianne.tensor.native/jni/cudnn/CUDNNModuleOps.c
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * DIANNE  - Framework for distributed artificial neural networks
+ * Copyright (C) 2015  iMinds - IBCN - UGent
+ *
+ * This file is part of DIANNE.
+ *
+ * DIANNE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *     Tim Verbelen, Steven Bohez
+ *******************************************************************************/
+#include "be_iminds_iot_dianne_tensor_ModuleOps.h"
+#include "TensorLoader.h"
+
+
+JNIEXPORT jobject JNICALL Java_be_iminds_iot_dianne_tensor_ModuleOps_spatialconvolve
+  (JNIEnv * env, jclass c, jobject out, jobject in, jobject ker, jobject b, jobject t1, jobject t2, jint kW, jint kH, jint dW, jint dH, jint pW, jint pH){
+	THTensor* input = getTensor(env, in);
+	THTensor* output = getTensor(env, out);
+	THTensor* weight = getTensor(env, ker);
+	THTensor* bias = getTensor(env, b);
+
+	printf("Spatial convolve forward in CUDNN! \n");
+
+	return out == NULL ? createTensorObject(env, output) : out;
+}
+
+JNIEXPORT jobject JNICALL Java_be_iminds_iot_dianne_tensor_ModuleOps_spatialconvolveGradIn
+  (JNIEnv * env, jclass c, jobject gradIn, jobject gradOut, jobject ker, jobject in, jobject t1, jobject t2, jint kW, jint kH, jint dW, jint dH, jint pW, jint pH){
+	THTensor* gradInput = getTensor(env, gradIn);
+	THTensor* gradOutput = getTensor(env, gradOut);
+	THTensor* weight = getTensor(env, ker);
+	THTensor* input = getTensor(env, in);
+	THTensor* temp1 = getTensor(env, t1);
+	THTensor* temp2 = getTensor(env, t2);
+
+	printf("Spatial convolve backward in CUDNN! \n");
+
+	return gradIn == NULL ? createTensorObject(env, gradInput) : gradIn;
+}
+
+JNIEXPORT void JNICALL Java_be_iminds_iot_dianne_tensor_ModuleOps_spatialconvolveAccGrad
+  (JNIEnv * env, jclass c, jobject gradKer, jobject gradB, jobject gradOut, jobject in, jobject t1, jobject t2, jint kW, jint kH, jint dW, jint dH, jint pW, jint pH){
+	THTensor* gradWeight = getTensor(env, gradKer);
+	THTensor* gradBias = getTensor(env, gradB);
+	THTensor* gradOutput = getTensor(env, gradOut);
+	THTensor* input = getTensor(env, in);
+	THTensor* temp1 = getTensor(env, t1);
+	THTensor* temp2 = getTensor(env, t2);
+
+	printf("Spatial convolve accgrad in CUDNN! \n");
+
+}
+
+
