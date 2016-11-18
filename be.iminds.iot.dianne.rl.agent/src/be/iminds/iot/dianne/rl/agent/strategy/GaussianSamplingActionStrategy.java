e06a7731d6334f79a55778f6b1f7f4982587b6c5
Steven Bohez <stevenbohez@gmail.com>
2016-11-18 11:47:23 +0100

    Added capping to gaussian sampling strategy

--- be.iminds.iot.dianne.rl.agent/src/be/iminds/iot/dianne/rl/agent/strategy/GaussianSamplingActionStrategy.java
+++ be.iminds.iot.dianne.rl.agent/src/be/iminds/iot/dianne/rl/agent/strategy/GaussianSamplingActionStrategy.java
@@ -6,6 +6,8 @@ import be.iminds.iot.dianne.api.nn.NeuralNetwork;
 import be.iminds.iot.dianne.api.rl.agent.ActionStrategy;
 import be.iminds.iot.dianne.api.rl.agent.AgentProgress;
 import be.iminds.iot.dianne.api.rl.environment.Environment;
+import be.iminds.iot.dianne.nn.util.DianneConfigHandler;
+import be.iminds.iot.dianne.rl.agent.strategy.config.GaussianNoiseConfig;
 import be.iminds.iot.dianne.tensor.Tensor;
 import be.iminds.iot.dianne.tensor.TensorOps;
 
@@ -13,12 +15,15 @@ public class GaussianSamplingActionStrategy implements ActionStrategy {
 
 	private NeuralNetwork policy;
 	
+	private GaussianNoiseConfig config;
+	
 	private int actionDims;
 	private Tensor action;
 	
 	@Override
 	public void setup(Map<String, String> config, Environment env, NeuralNetwork... nns) throws Exception {
 		this.policy = nns[0];
+		this.config = DianneConfigHandler.getConfig(config, GaussianNoiseConfig.class);
 		this.actionDims = env.actionDims()[0];
 		this.action = new Tensor(this.actionDims);
 	}
@@ -34,6 +39,14 @@ public class GaussianSamplingActionStrategy implements ActionStrategy {
 		
 		TensorOps.cmul(action, action, stdevs);
 		TensorOps.add(action, action, means);
+		
+		for(int a = 0; a < action.size(); a++) {
+			float v = action.get(a);
+			if(v < config.min)
+				action.set(config.min, a);
+			else if(v > config.max)
+				action.set(config.max, a);
+		}
 		
 		return new AgentProgress(i, action);
 	}
