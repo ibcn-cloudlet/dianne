1cc7d333f0cdd8c3d2dfce41702768dad5846483
Tim Verbelen <tim.verbelen@intec.ugent.be>
2016-12-13 15:35:56 +0100

    add an EnergyPenalizationAdapter

---
+++ be.iminds.iot.dianne.rl.experience/src/be/iminds/iot/dianne/rl/experience/adapters/EnergyPenalizationAdapter.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * DIANNE  - Framework for distributed artificial neural networks
+ * Copyright (C) 2015  iMinds - IBCN - UGent
+ *
+ * This file is part of DIANNE.
+ *
+ * DIANNE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *     Tim Verbelen, Steven Bohez
+ *******************************************************************************/
+package be.iminds.iot.dianne.rl.experience.adapters;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+
+import be.iminds.iot.dianne.api.dataset.Dataset;
+import be.iminds.iot.dianne.api.rl.dataset.ExperiencePool;
+import be.iminds.iot.dianne.api.rl.dataset.ExperiencePoolSample;
+import be.iminds.iot.dianne.tensor.TensorOps;
+
+@Component(
+		service={Dataset.class, ExperiencePool.class},	
+		configurationPolicy=ConfigurationPolicy.REQUIRE,
+		configurationPid="be.iminds.iot.dianne.dataset.adapters.EnergyPenalizationAdapter")
+public class EnergyPenalizationAdapter extends AbstractExperiencePoolAdapter {
+
+	private float energyPenalization = 0.0f;
+	
+	@Override
+	protected void configure(Map<String, Object> properties) {
+		if(properties.containsKey("energyPenalization")){
+			this.energyPenalization = Float.parseFloat(properties.get("energyPenalization").toString());
+		}
+	}
+
+	@Override
+	protected void adaptFetchedSample(ExperiencePoolSample s) {
+		TensorOps.sub(s.reward, s.reward, energyPenalization*TensorOps.dot(s.target, s.target));
+	}
+
+	@Override
+	protected void adaptAddingSample(ExperiencePoolSample s) {
+	}
+
+
+
+}
