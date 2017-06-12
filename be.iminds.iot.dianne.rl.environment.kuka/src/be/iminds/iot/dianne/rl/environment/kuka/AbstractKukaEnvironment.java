4ae164771c05b11949334389ef7aafc5330a8b71
Tim Verbelen <tim.verbelen@intec.ugent.be>
2017-06-12 13:31:27 +0200

    use pkill instead of killall

--- be.iminds.iot.dianne.rl.environment.kuka/src/be/iminds/iot/dianne/rl/environment/kuka/AbstractKukaEnvironment.java
+++ be.iminds.iot.dianne.rl.environment.kuka/src/be/iminds/iot/dianne/rl/environment/kuka/AbstractKukaEnvironment.java
@@ -348,7 +348,7 @@ public abstract class AbstractKukaEnvironment implements Environment, KukaEnviro
 			} catch(Exception e){
 				// try to kill the simulator?! - this is hacky!
 				// TODO this should be fixed in the robot project?
-				Runtime.getRuntime().exec("killall vrep");
+				Runtime.getRuntime().exec("pkill vrep");
                 simulator = null;
             	System.out.println("Unexpected simulator error, waiting for simulator to come back online...");
 				long start = System.currentTimeMillis();
