2158b175be2e02266307e07ef34f271ce7b6ea98
Tim Verbelen <tim.verbelen@intec.ugent.be>
2017-08-30 15:39:10 +0200

    also redirect root to /dianne/builder

---
+++ be.iminds.iot.dianne.builder/src/be/iminds/iot/dianne/builder/DianneRedirectRoot.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * DIANNE  - Framework for distributed artificial neural networks
+ * Copyright (C) 2015  iMinds - IBCN - UGent
+ *
+ * This file is part of DIANNE.
+ *
+ * DIANNE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *     Tim Verbelen, Steven Bohez
+ *******************************************************************************/
+package be.iminds.iot.dianne.builder;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(service={javax.servlet.Servlet.class},
+       property={"alias:String=/",
+                         "osgi.http.whiteboard.servlet.pattern=/",
+                         "aiolos.proxy=false"},
+       immediate=true)
+public class DianneRedirectRoot extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+       protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+               resp.sendRedirect("/dianne/builder");
+       }
+
+}
