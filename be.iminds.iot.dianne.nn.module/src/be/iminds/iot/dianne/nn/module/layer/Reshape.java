2ee6465514e65a3450a54435ff06da9f23965eb0
Tim Verbelen <tim.verbelen@intec.ugent.be>
2018-02-14 21:22:44 +0100

    cleanup

--- be.iminds.iot.dianne.nn.module/src/be/iminds/iot/dianne/nn/module/layer/Reshape.java
+++ be.iminds.iot.dianne.nn.module/src/be/iminds/iot/dianne/nn/module/layer/Reshape.java
@@ -64,12 +64,7 @@ public class Reshape extends AbstractModule {
 		if(inputSize != targetSize){
 			if(inputSize / inputDims[0] == targetSize){
 				// batch dimension
-				int[] newDim = new int[targetDims.length + 1];
-				newDim[0] = inputDims[0];
-				for(int i=0;i<targetDims.length;i++){
-					newDim[i+1] = targetDims[i];
-				}
-				output.reshape(newDim);
+				output.reshape(inputDims[0], targetDims);
 			} else {
 				throw new RuntimeException("Invalid input dimensions to reshape?! input dims : " +Arrays.toString(input.dims())+" target dims: "+Arrays.toString(targetDims));
 			}
