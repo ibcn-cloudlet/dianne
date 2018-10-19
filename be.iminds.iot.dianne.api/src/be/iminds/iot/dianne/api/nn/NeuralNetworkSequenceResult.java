9cf5e8fab6d1aef2713cd10be04d6d9ac7eeceb1
Tim Verbelen <tim.verbelen@intec.ugent.be>
2018-10-19 14:23:32 +0200

    cleanup

--- be.iminds.iot.dianne.api/src/be/iminds/iot/dianne/api/nn/NeuralNetworkSequenceResult.java
+++ be.iminds.iot.dianne.api/src/be/iminds/iot/dianne/api/nn/NeuralNetworkSequenceResult.java
@@ -22,7 +22,6 @@
  *******************************************************************************/
 package be.iminds.iot.dianne.api.nn;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
